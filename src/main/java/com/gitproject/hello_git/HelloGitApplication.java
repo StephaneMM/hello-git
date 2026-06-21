3cb4203e10a533b2bdec6cf1885c14777bf15a0f
Stephane MM <stephane.mbesse@gmail.com>
2026-06-21 15:30:14 -0700

    initialized project

---
+++ src/main/java/com/gitproject/hello_git/HelloGitApplication.java
@@ -0,0 +1,9 @@
+package com.gitproject.hello_git;
+
+
+public class HelloGitApplication {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    
+    }
+}
